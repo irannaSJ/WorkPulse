672cebc41121803d218ebfc5f0f2e7e10f5e6499
Iranna S J <irannasj2004@gmail.com>
2026-08-11 12:05:25 +0530

    branch switch

--- app/src/main/java/com/example/workpulse/core/network/NetworkConstants.kt
+++ app/src/main/java/com/example/workpulse/core/network/NetworkConstants.kt
@@ -2,11 +2,13 @@ package com.example.workpulse.core.network
 
 object NetworkConstants {
 
-    const val BASE_URL = "http://10.205.75.19:8000"
+//    const val BASE_URL = "http://10.205.75.19:8000"
 //    const val BASE_URL = "http://10.163.155.19:8000"
 //    const val BASE_URL = "http://10.132.113.19:8000"
 //    const val BASE_URL = "http://10.159.58.19:8000"
 //    const val BASE_URL = "http://10.139.76.19:8000"
+const val BASE_URL = "https://opening-advantages-rewards-arc.trycloudflare.com/"
+
 
 
     const val LOGIN = "api/method/login"
